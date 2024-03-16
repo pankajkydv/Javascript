06658098a852bb6f1ed911a3d8910b8a8850c4e4
Pankaj Yadav <42650848+pankajkydv@users.noreply.github.com>
2024-03-16 10:00:12 +0000

    datatypes conversions

--- 01_basics/03_dataypesConversion.js
+++ 01_basics/03_dataypesConversion.js
@@ -2,20 +2,34 @@ let score="123"
 console.log(score)
 console.log(typeof(score))
 // console.log(typeof score)
+console.log("********Number conversion*******");
 let valueInNumber=Number(score)
 console.log(typeof(valueInNumber))
 console.log(valueInNumber)
 // 33>=number "33ab">=NAN  
 //  True>=1 false>=0
 // null>=0 undefined>=NaN
-
+console.log("*********Boolean Conversion********")
 let isLoggedIn=undefined
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn)
 // 1>=true 0>=false "">=false 01ab>=true
 // String>=true null>=false  undefined>=false
-
+console.log("****String conversion********")
 let someNumber=null
 let stringNumber=String(someNumber)
 console.log(stringNumber);
 console.log(typeof(stringNumber));
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+"2")
+console.log(1+2+"3");
+console.log("1"+2+3)
+
+console.log("*****Postfix and infix operators********");
+let a=1
+let b=10
+let x=++a
+let y=b++
+console.log("value of a and b are "+a+ " "+b)
+console.log("value of x and y are "+x+" "+y )
