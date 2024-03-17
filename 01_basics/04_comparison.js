f1f52beecd8d9a5627f5e2345fc947a98e6f017e
Pankaj Yadav <42650848+pankajkydv@users.noreply.github.com>
2024-03-17 17:04:38 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+console.log("2">1);
+console.log("02">1);
+console.log("******Null comparison*******");
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+console.log(null>=0);  //in converts the null to zero null>=0 true
+console.log("****Undefined Comparison*******");
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined<=0);
+
+console.log("2"===2);
